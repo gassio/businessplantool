23c3d149cf3a71834c59c3556693c662be11409d
gassio <gassiogi@gmail.com>
2018-07-06 13:37:18 +0300

    change 6

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(serveStatic(path.join(__dirname, './dist')));
 // app.use(express.static(__dirname + "/dist"));
-app.use(serveStatic('dist'));
+// app.use(serveStatic('dist'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+
+// '0.0.0.0',
 
 // app.get('/*', function(req, res) {
 //     res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
