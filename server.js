fe9762ee022cef08da09df7a00001109fb640dc3
gassio <gassiogi@gmail.com>
2018-07-06 13:40:16 +0300

    change 7

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(serveStatic(path.join(__dirname, './dist')));
-// app.use(express.static(__dirname + "/dist"));
+// app.use(serveStatic(path.join(__dirname, './dist')));
+app.use(express.static(__dirname + "/dist"));
 // app.use(serveStatic('dist'));
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), '0.0.0.0', function() {
     console.log('Node app is running on port', app.get('port'));
 });
-
-// '0.0.0.0',
 
 // app.get('/*', function(req, res) {
 //     res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
