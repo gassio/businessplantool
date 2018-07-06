44555bd282e09858d0a00bc1cb3d02e0c74db86d
gassio <gassiogi@gmail.com>
2018-07-06 13:59:26 +0300

    Now the refresh is working... muhahaha

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.listen(app.get('port'), '0.0.0.0', function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
-//         if (err) {
-//           res.status(500).send(err)
-//         }
-//       })
-// })
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
+        if (err) {
+          res.status(500).send(err)
+        }
+      })
+})
